e664163e382601bed05c5fd00dafd763e5d7efd0
Amilelne <1433021647@qq.com>
2023-07-03 03:49:45 +0000

    Deploying to gh-pages from @ Amilelne/joy-tool@f10aef002fd0f136a3ee6d873345bfa946934bee 🚀

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  devServer: {
-    proxy: 'http://localhost:4000',
-    open: true,
-    hot: true
-  }
-})
